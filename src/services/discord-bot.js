ed1a855111395f4a4173f25396cc9ba7088b8789
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2024-06-27 13:25:22 +0700

    avatar

--- src/services/discord-bot.js
+++ src/services/discord-bot.js
@@ -1,8 +1,9 @@
+// Load environment variables from .env file
 require("dotenv").config({
   path: require("path").resolve(__dirname, "../../.env"),
-}); // Load environment variables from .env file
+});
 
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js"); // Import Client, GatewayIntentBits, and EmbedBuilder
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 
 // Initialize the client with the correct intent flag
 const client = new Client({
@@ -16,7 +17,7 @@ const client = new Client({
 
 // Event handler for when the bot is ready
 client.on("ready", () => {
-  console.log("\x1b[1mBot\x1b[0m is now \x1b[32mOnline\x1b[0m!"); // \x1b[1m sets text to bold, \x1b[0m resets text style
+  console.log("\x1b[1mBot\x1b[0m is now \x1b[32mOnline\x1b[0m!");
 });
 
 // Event handler for when an interaction is created
@@ -28,8 +29,6 @@ client.on("interactionCreate", async (interaction) => {
         content: `🏓 Pong! ${ping}ms`,
         ephemeral: true,
       });
-
-      // Log interaction details to console
       console.log(
         `\x1b[1mBot\x1b[0m: Command /ping used by ${interaction.user.tag} in ${interaction.guild.name}`
       );
@@ -37,19 +36,21 @@ client.on("interactionCreate", async (interaction) => {
       const user = interaction.options.getUser("user") || interaction.user;
       const member = await interaction.guild.members.fetch(user.id);
       const roleColor = member.displayHexColor;
-      const avatarURL = user.displayAvatarURL({ dynamic: true, size: 1024 });
+      const avatarURL = user.displayAvatarURL({
+        dynamic: true,
+        size: 1024,
+      });
 
       const embed = new EmbedBuilder()
         .setTitle(`${member.displayName}`)
         .setImage(avatarURL)
-        .setColor(roleColor !== "#000000" ? roleColor : "#aa0000"); // Use the role color if available, otherwise default to #aa0000
+        .setColor(roleColor !== "#000000" ? roleColor : "#aa0000");
 
       await interaction.reply({
         embeds: [embed],
         ephemeral: true,
       });
 
-      // Log interaction details to console
       console.log(
         `\x1b[1mBot\x1b[0m: Command /avatar used by ${interaction.user.tag} in ${interaction.guild.name}`
       );
